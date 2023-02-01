fe3c8e9c926b47a0b7e8ccc7999d17f78a1f4515
Omkar141998 <onagpure688@gmail.com>
2023-02-01 22:14:13 +0530

    run

---
+++ file.js
@@ -0,0 +1,25 @@
+//Image Form
+const imageform = document.getElementById("image-form");
+
+function openImageform(){
+  imageform.classList.add("open-Imageform");
+}
+function closeImageform(){
+  imageform.classList.remove("open-Imageform");
+};
+ 
+//dice game
+let diceImage = document.getElementsById("dice-image");
+
+function openDiceimage(){
+diceImage.classList.add("open-Diceimage");
+}
+function closeDiceimage(){
+diceImage.classList.remove("open-Diceimage");
+}
+
+
+
+
+
+
